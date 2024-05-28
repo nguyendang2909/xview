50a9cf957e81564e2cdd744ffe72c008bf0d0915
nguyendang2909 <nguyendang2909@gmail.com>
2024-05-28 23:21:04 +0700

    feat: app rename

--- ios/XviewStoreTests/XviewStoreTests.m
+++ ios/XviewStoreTests/XviewStoreTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface xviewTests : XCTestCase
+@interface XviewStoreTests : XCTestCase
 
 @end
 
-@implementation xviewTests
+@implementation XviewStoreTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
